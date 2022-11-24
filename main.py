ce0e49462cb62394be221cd38416e5f01fa406ad
davidgfb <davidgracia99@gmail.com>
2022-11-24 12:28:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import pygame as pg
 import moderngl as mgl
 import sys
 from model import *
@@ -7,29 +6,42 @@ from light import Light
 from mesh import Mesh
 from scene import Scene
 from scene_renderer import SceneRenderer
-
+from pygame import init, GL_CONTEXT_MAJOR_VERSION, GL_CONTEXT_MINOR_VERSION,\
+     GL_CONTEXT_PROFILE_MASK, GL_CONTEXT_PROFILE_CORE, OPENGL, DOUBLEBUF, QUIT,\
+     KEYDOWN, K_ESCAPE, quit
+from pygame.display import gl_set_attribute, set_mode, flip
+from pygame.event import set_grab, get
+from pygame.mouse import set_visible
+from pygame.time import Clock, get_ticks
+from numpy import array
 
 class GraphicsEngine:
-    def __init__(self, win_size=(1600, 900)):
+    def __init__(self, win_size = (1600, 900)):
         # init pygame modules
-        pg.init()
+        init()
         # window size
         self.WIN_SIZE = win_size
         # set opengl attr
-        pg.display.gl_set_attribute(pg.GL_CONTEXT_MAJOR_VERSION, 3)
-        pg.display.gl_set_attribute(pg.GL_CONTEXT_MINOR_VERSION, 3)
-        pg.display.gl_set_attribute(pg.GL_CONTEXT_PROFILE_MASK, pg.GL_CONTEXT_PROFILE_CORE)
+
+        (*(gl_set_attribute(a, b) for a, b in ((GL_CONTEXT_MAJOR_VERSION, 3),\
+                                      (GL_CONTEXT_MINOR_VERSION, 3),\
+                                      (GL_CONTEXT_PROFILE_MASK,\
+                                       GL_CONTEXT_PROFILE_CORE))),)
+        
+        '''gl_set_attribute(GL_CONTEXT_MAJOR_VERSION, 3)
+        gl_set_attribute(GL_CONTEXT_MINOR_VERSION, 3)
+        gl_set_attribute(GL_CONTEXT_PROFILE_MASK, GL_CONTEXT_PROFILE_CORE)'''
         # create opengl context
-        pg.display.set_mode(self.WIN_SIZE, flags=pg.OPENGL | pg.DOUBLEBUF)
+        set_mode(self.WIN_SIZE, flags = OPENGL | DOUBLEBUF)
         # mouse settings
-        pg.event.set_grab(True)
-        pg.mouse.set_visible(False)
+        set_grab(True)
+        set_visible(False)
         # detect and use existing opengl context
         self.ctx = mgl.create_context()
         # self.ctx.front_face = 'cw'
-        self.ctx.enable(flags=mgl.DEPTH_TEST | mgl.CULL_FACE)
+        self.ctx.enable(flags = mgl.DEPTH_TEST | mgl.CULL_FACE)
         # create an object to help track time
-        self.clock = pg.time.Clock()
+        self.clock = Clock()
         self.time = 0
         self.delta_time = 0
         # light
@@ -44,23 +56,24 @@ class GraphicsEngine:
         self.scene_renderer = SceneRenderer(self)
 
     def check_events(self):
-        for event in pg.event.get():
-            if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
+        for event in get():
+            if event.type == QUIT or event.type == KEYDOWN and\
+                                      event.key == K_ESCAPE:
                 self.mesh.destroy()
                 self.scene_renderer.destroy()
-                pg.quit()
+                quit()
                 sys.exit()
 
     def render(self):
         # clear framebuffer
-        self.ctx.clear(color=(0.08, 0.16, 0.18))
+        self.ctx.clear(color = array((4, 8, 9)) / 50)
         # render scene
         self.scene_renderer.render()
         # swap buffers
-        pg.display.flip()
+        flip()
 
     def get_time(self):
-        self.time = pg.time.get_ticks() * 0.001
+        self.time = get_ticks() / 1000
 
     def run(self):
         while True:
@@ -69,7 +82,6 @@ class GraphicsEngine:
             self.camera.update()
             self.render()
             self.delta_time = self.clock.tick(60)
-
 
 if __name__ == '__main__':
     app = GraphicsEngine()
