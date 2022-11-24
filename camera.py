2164cf04cc15438d0a03448b35e7861619edef7d
davidgfb <davidgracia99@gmail.com>
2022-11-24 14:28:25 +0100

    Revert

--- camera.py
+++ camera.py
@@ -39,43 +39,31 @@ class Camera:
 
     def move(self):
         velocity = SPEED * self.app.delta_time
-        keys = get_pressed()      
-        teclas = {keys[K_w] : self.forward, keys[K_d] : self.right,\
-                  keys[K_q] : self.up, keys[K_s] : -self.forward,\
-                  keys[K_a] : -self.right, keys[K_e] : -self.up}
+        keys = get_pressed()
 
-        '''teclas = {1 : (keys[K_w], keys[K_d], keys[K_q]),\
-                  -1 : (keys[K_s], keys[K_a], keys[K_e])}'''
+        if keys[K_w]:
+            self.position += self.forward * velocity
 
-        '''teclas = {keys[K_w] or keys[K_s] : self.forward,\
-                  keys[K_d] or keys[K_a] : self.right,\
-                  keys[K_q] or keys[K_e] : self.up}'''
-
-        for tecla_Pos in teclas:
-            if tecla_Pos:
-                if keys[K_w] and keys[K_d]:
-                    velocity *= self.forward + self.right
-
-                elif keys[K_w] and keys[K_a]:
-                    velocity *= self.forward - self.right
-
-                elif keys[K_a] and keys[K_s]:
-                    velocity *= -self.forward - self.right
-
-                elif keys[K_s] and keys[K_d]:
-                    velocity *= self.right - self.forward
-
-                else:
-                    velocity *= teclas[tecla_Pos]
-
-                self.position += velocity
+        if keys[K_d]:
+            self.position += self.right * velocity
+            
+        if keys[K_q]:
+            self.position += self.up * velocity
+            
+        if keys[K_s]:
+            self.position -= self.forward * velocity
+            
+        if keys[K_a]:
+            self.position -= self.right * velocity
+                     
+        if keys[K_e]:
+            self.position -= self.up * velocity
 
     def get_view_matrix(self):
         return lookAt(self.position, self.position + self.forward, self.up)
 
     def get_projection_matrix(self):
         return perspective(radians(FOV), self.aspect_ratio, NEAR, FAR)
-
 
 
 
